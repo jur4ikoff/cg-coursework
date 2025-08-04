33ea7219011a6927122351f29aed766b9c98b853
1 <ypopov2005@gmail.com>
2025-08-04 22:44:32 +0300

    update

--- src/main_window.h
+++ src/main_window.h
@@ -1,33 +1,54 @@
-#ifndef MAINWINDOW_H__
-#define MAINWINDOW_H__
+#pragma once
 
+#include "ui_main_window.h"
+
+#include <QApplication>
 #include <QMainWindow>
-#include <iostream>
+#include <QHBoxLayout>
+#include <QLabel>
+#include <QPixmap>
+#include <QWidget>
 
-#define WINDOWS_X 1400
-#define WINDOWS_Y 750
+#define DEFAULT_WIDTH 1200
+#define DEFAULT_HEIGHT 800
 
-#define SCENE_SIZE 700
-
+#define MIN_SCENE_WIDTH 700
+#define MIN_SCENE_HEIGHT 600
 QT_BEGIN_NAMESPACE
 
 namespace Ui
 {
     class MainWindow;
 }
-
 QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
-
 public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow(void);
+    MainWindow(QWidget *parent = nullptr) : QMainWindow(parent), ui(new Ui::MainWindow)
+    {
+        ui->setupUi(this); // Загрузка UI-формы (обязательно!)
+
+        // Настройка главного окна
+        setWindowTitle("Фиксированный QPixmap слева (C++)");
+        // resize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+
+        ui->imageLabel->setMinimumSize(MIN_SCENE_WIDTH, MIN_SCENE_HEIGHT); // Фиксированный размер
+        this->ui->imageLabel->setAlignment(Qt::AlignCenter);
+
+        // // Загрузка изображения
+        QPixmap pixmap("./../image.png"); // Используйте ваш путь
+        if (!pixmap.isNull())
+        {
+            ui->imageLabel->setPixmap(pixmap);
+        }
+        else
+        {
+            ui->imageLabel->setText("Изображение не загружено");
+        }
+    }
 
 private:
     Ui::MainWindow *ui;
 };
-
-#endif // MAINWINDOW_H__
