b92f0264c8cc678f05b3d300531037fae94d5335
1 <ypopov2005@gmail.com>
2025-09-18 10:21:58 +0300

    add circle render

--- rays/ray.h
+++ rays/ray.h
@@ -23,7 +23,7 @@ private:
     Vec3 _direction;
 };
 
-Color ray_color(Ray r)
+Color sky(Ray r)
 {
     // Приведение к единичному вектору
     Vec3 unit_direction = unit_vector(r.direction());
@@ -32,4 +32,29 @@ Color ray_color(Ray r)
     // По формуле линейной интерполяци
     // blended_value = (1 - a) * start_value + a * end_value
     return (1.0 - a) * Color(1.0, 1.0, 1.0) + a * Color(0.5, 0.7, 1.0);
+}
+
+bool hit_sphere(const Point3 &center, double radius, const Ray &r)
+{
+    // Решаем квадраное уравнение t^2 * d - 2td * (C - Q) + (C - Q) * (C - Q) - r^2 =0
+    // Вектор между началом луча и центром окруност
+    Vec3 oc = center - r.origin();
+
+    // Скалряное проивзедения направления вектора на самого себя
+    double a = dot(r.direction(), r.direction());
+    double b = -2.0 * dot(r.direction(), oc);
+    double c = dot(oc, oc) - radius * radius;
+
+    double discriminant = b * b - 4 * a * c;
+    return (discriminant >= 0);
+}
+
+Color ray_color(Ray r)
+{
+    if (hit_sphere(Point3(0, 0, 1), 0.5, r))
+    {
+        return Color(1, 0, 0);
+    }
+
+    return sky(r);
 }
