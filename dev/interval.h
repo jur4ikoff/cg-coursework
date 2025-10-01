7cc26b3751ea9d14738036279fd92a1806402478
1 <ypopov2005@gmail.com>
2025-10-02 02:30:11 +0300

    update

--- dev/interval.h
+++ dev/interval.h
@@ -2,12 +2,13 @@
 
 #include <limits>
 const double INF = std::numeric_limits<double>::infinity();
-const double PI = M_PI; 
+const double PI = M_PI;
 
 class Interval
 {
 public:
-    double min, max;
+    double min,
+        max;
     Interval() : min(INF), max(-INF) {}
     Interval(double min, double max) : min(min), max(max) {}
     double size() const
@@ -24,8 +25,15 @@ public:
     {
         return min < x && x < max;
     }
+
+    double clamp(double x) const
+    {
+        if (x < min)
+            return min;
+        if (x > max)
+            return max;
+        return x;
+    }
+
     static const Interval empty, universe;
 };
-
-const Interval Interval::empty = Interval(INF, -INF);
-const Interval Interval::universe = Interval(-INF, INF);
